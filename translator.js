9adbb98bcf3a8cc999074a142ae748d238b1a516
Dmytro Ursul <cartobus@gmail.com>
2022-07-30 11:25:39 +0300

    translate with cmd key

--- translator.js
+++ translator.js
@@ -8,109 +8,49 @@ document.documentElement.insertBefore(link, document.documentElement.firstChild)
 let cx;
 let cy;
 
-document.onmouseup = function(e){
-  const fp = document.getElementById('notes_float_panel');
+document.ondblclick = function(e){
   const selection = window.getSelection().toString();
-  if ( check(selection, fp) ){
-    cx = e.clientX - 50;
-    cy = e.clientY - 30 + document.body.scrollTop;
-    popup.invoke(cx, cy);
-    popup.getSel(selection);
-  }
+  cx = e.clientX - 50;
+  cy = e.clientY - 30 + window.scrollY;
+  if (selection.length && e.metaKey) sendData(selection);
 }
 
-function remove_panel(fp){
-  if (!fp) return;
-
-  fp.removeEventListener("mousedown", popup.sendData);
-  document.body.removeChild(fp);
-}
-
-function check(selection,fp){
-  if (selection.length && fp){
-    if (selection != popup.data){
-      remove_panel(fp);
-      return true;
-    }
-    remove_panel(fp);
-    return;
-  }
-  else if (selection.length && !fp){
-    return true;
-  }
-  else if (!selection.length && fp){
-    remove_panel(fp);
-    return;
-  }
-  else if (!selection.length && !fp){
-    return;
-  }
-  else
-    return true;
-}
-// function: remove element with id "notes_float_panel_translation" on click somewhere on page
-document.onclick = function(e) {   // при клике на странице
+document.onclick = function(e) {
   const fp = document.getElementById('notes_float_panel_translation');
-  remove_panel(fp);
+  fp && document.body.removeChild(fp);
 }
 
-const popup = {
-  data: "",
+const showTranslation = function(data){
+  const div = document.createElement('div');
 
-  invoke: function(x,y){
-    const fp = document.createElement('img');
-
-    fp.id = 'notes_float_panel';
-    fp.src = browser.runtime.getURL("edit.png");
-    fp.style.cssText = 'left:'+x+'px;\
-                        top:' +y+'px;';
-    fp.addEventListener("mousedown", popup.sendData);
-
-    document.body.appendChild(fp);
-  },
-
-  getSel: function(data){
-    this.data = data;
-  },
-
-  showTranslation: function(data){
-    const div = document.createElement('div');
-
-    div.id = 'notes_float_panel_translation';
-    div.innerHTML = data;
-    div.style.cssText = 'left:'+cx+'px;\
+  div.id = 'notes_float_panel_translation';
+  div.innerHTML = data;
+  div.style.cssText = 'left:'+cx+'px;\
                          top:' +cy+'px;'
 
-    document.body.appendChild(div);
-    const fp = document.getElementById('notes_float_panel');
-    remove_panel(fp)
-  },
+  document.body.appendChild(div);
+};
 
-  sendData: function(ev){
-    ev.stopPropagation();
-    const fp = document.getElementById('notes_float_panel');
-    remove_panel(fp);
-
-    const data = {
-      'apiVersion': '1.0.1',
-      'op': 'getTranslates',
-      'text': popup.data,
-      'ctx': {
-        'config': {
-          'isCheckData': true,
-          'isLogging': true
-        }
+const sendData = function(selection){
+  const data = {
+    'apiVersion': '1.0.1',
+    'op': 'getTranslates',
+    'text': selection,
+    'ctx': {
+      'config': {
+        'isCheckData': true,
+        'isLogging': true
       }
     }
-
-    const request = new Request(LEO_API_URL, {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify(data)
-    });
-
-    fetch(request).
-      then(response => response.json()).
-      then(resp => popup.showTranslation(resp.translation));
   }
+
+  const request = new Request(LEO_API_URL, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(data)
+  });
+
+  fetch(request).
+    then(response => response.json()).
+    then(resp => showTranslation(resp.translation));
 }
